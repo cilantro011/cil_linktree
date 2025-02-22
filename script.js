8b989ff04ee2ef928ca40b6d450be2e4615ccf72
Sajan Tamang <sajaanlaama@gmail.com>
2025-02-22 10:14:24 -0600

    created js to fetch data from flask

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-*{
-    margin: 0;
-    padding; 0;
-    box-sizing: border-box;
+async function fetchFlaskData(){
+    try {
+        const response = await fetch("http://127.0.0.1:5000/");
+        const data = await response.json();
+
+        document.getElementById("spotify-now-playing").innerText = data.message;
+    } catch (error) {
+        console.error("error fetching data:", error);
+    }
 }
+
+fetchFlaskData();
+
